194bb6168c58d306215839c1d03ca1dd4231add2
Yunchu <33641269+YunchuZhang@users.noreply.github.com>
2018-06-18 19:33:06 -0700

    Add files via upload

---
+++ Picamera.py
@@ -0,0 +1,263 @@
+# import the necessary packages
+import os
+
+if os.name == 'nt':
+        import msvcrt
+        def getch():
+                return msvcrt.getch().decode()
+else:
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        def getch():
+                try:
+                        tty.setraw(sys.stdin.fileno())
+                        ch = sys.stdin.read(1)
+                finally:
+                        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+                return ch
+
+# Control table address
+ADDR_PRO_TORQUE_ENABLE      = 24              # Control table address is different in Dynamixel model
+ADDR_PRO_GOAL_POSITION      = 30
+ADDR_PRO_PRESENT_POSITION   = 37
+
+# Protocol version
+PROTOCOL_VERSION            = 2.0               # See which protocol version is used in the Dynamixel
+
+# Default setting
+DXL_ID                      = 11                 # Dynamixel ID : 1
+BAUDRATE                    = 1000000             # Dynamixel default baudrate : 57600
+DEVICENAME                  = '/dev/ttyUSB0'    # Check which port is being used on your controller
+                                                # ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
+
+TORQUE_ENABLE               = 1                 # Value for enabling the torque
+TORQUE_DISABLE              = 0                 # Value for disabling the torque
+# mid 515
+DXL_MINIMUM_POSITION_VALUE  = 206          # Dynamixel will rotate between this value
+DXL_MAXIMUM_POSITION_VALUE  = 824            # and this value (note that the Dynamixel would not move when the position value is out of movable range. Check e-manual about the range of the Dynamixel you use.)
+DXL_MOVING_STATUS_THRESHOLD = 20                # Dynamixel moving status threshold
+
+
+
+from dynamix import *
+from collections import deque
+import numpy as np
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import argparse
+import imutils
+import time
+import cv2
+# HSV parameters
+# Hmin 138 Smin 155 Vmin 125
+# Hmax 175 Smax 255 Vmax 255
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video",
+	help="path to the (optional) video file")
+ap.add_argument("-b", "--buffer", type=int, default=64,
+	help="max buffer size")
+args = vars(ap.parse_args())
+
+
+# define the lower and upper boundaries of the "green"
+# ball in the HSV color space, then initialize the
+# list of tracked points
+
+redLower = (138, 155, 125)
+redUpper = (175, 255, 255)
+pts = deque(maxlen=args["buffer"])
+counter = 0
+(dX, dY) = (0, 0)
+direction = ""
+
+
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 45
+rawCapture = PiRGBArray(camera, size=(640, 480))
+
+
+# Goal position
+dxl_goal_position =515 
+
+
+# Initialize PortHandler instance
+# Set the port path
+# Get methods and members of PortHandlerLinux or PortHandlerWindows
+portHandler = PortHandler(DEVICENAME)
+
+# Initialize PacketHandler instance
+# Set the protocol version
+# Get methods and members of Protocol1PacketHandler or Protocol2PacketHandler
+packetHandler = PacketHandler(PROTOCOL_VERSION)
+
+# Open port
+if portHandler.openPort():
+    print("Succeeded to open the port")
+else:
+    print("Failed to open the port")
+    print("Press any key to terminate...")
+    getch()
+    quit()
+
+
+# Set port baudrate
+if portHandler.setBaudRate(BAUDRATE):
+    print("Succeeded to change the baudrate")
+else:
+    print("Failed to change the baudrate")
+    print("Press any key to terminate...")
+    getch()
+    quit()
+
+# Enable Dynamixel Torque
+dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_PRO_TORQUE_ENABLE, TORQUE_ENABLE)
+if dxl_comm_result != COMM_SUCCESS:
+    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+elif dxl_error != 0:
+    print("%s" % packetHandler.getRxPacketError(dxl_error))
+else:
+    print("Dynamixel has been successfully connected")
+
+
+# allow the camera or video file to warm up
+time.sleep(0.2)
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	# grab the raw NumPy array representing the image, then initialize the timestamp
+	# and occupied/unoccupied text
+	image = frame.array
+
+
+	# resize the frame, blur it, and convert it to the HSV
+	# color space
+	
+	blurred = cv2.GaussianBlur(image, (11, 11), 0)
+	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+ 
+	# construct a mask for the color "green", then perform
+	# a series of dilations and erosions to remove any small
+	# blobs left in the mask
+	mask = cv2.inRange(hsv, redLower, redUpper)
+	mask = cv2.erode(mask, None, iterations=2)
+	mask = cv2.dilate(mask, None, iterations=2)
+        # find contours in the mask and initialize the current
+	# (x, y) center of the ball
+	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)
+	cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+	center = None
+ 
+	# only proceed if at least one contour was found
+	if len(cnts) > 0:
+		# find the largest contour in the mask, then use
+		# it to compute the minimum enclosing circle and
+		# centroid
+		c = max(cnts, key=cv2.contourArea)
+		((x, y), radius) = cv2.minEnclosingCircle(c)
+		M = cv2.moments(c)
+		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+ 
+		# only proceed if the radius meets a minimum size
+		if radius > 5:
+			# draw the circle and centroid on the frame,
+			# then update the list of tracked points
+			cv2.circle(image, (int(x), int(y)), int(radius),
+				(0, 255, 255), 2)
+			cv2.circle(image, center, 5, (0, 0, 255), -1)
+			pts.appendleft(center)
+
+	# loop over the set of tracked points
+	for i in range(1, len(pts)):
+		# if either of the tracked points are None, ignore
+		# them
+		if pts[i - 1] is None or pts[i] is None:
+			continue
+
+		# check to see if enough points have been accumulated in
+		# the buffer
+		if counter >= 10 and i == 1 and pts[-10] is not None:
+			# compute the difference between the x and y
+			# coordinates and re-initialize the direction
+			# text variables
+			dX = pts[-10][0] - pts[i][0]
+			dY = pts[-10][1] - pts[i][1]
+			(dirX, dirY) = ("", "")
+ 
+			# ensure there is significant movement in the
+			# x-direction
+			if np.abs(dX) > 20:
+				dirX = "East" if np.sign(dX) == 1 else "West"
+ 
+			# ensure there is significant movement in the
+			# y-direction
+			if np.abs(dY) > 20:
+				dirY = "North" if np.sign(dY) == 1 else "South"
+ 
+			# handle when both directions are non-empty
+			if dirX != "" and dirY != "":
+				direction = "{}-{}".format(dirY, dirX)
+ 
+			# otherwise, only one direction is non-empty
+			else:
+				direction = dirX if dirX != "" else dirY
+
+ 
+		# otherwise, compute the thickness of the line and
+		# draw the connecting lines
+		thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+		cv2.line(image, pts[i - 1], pts[i], (0, 0, 255), thickness)
+
+	# show the movement deltas and the direction of movement on
+	# the frame
+	cv2.putText(image, direction, (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
+		0.65, (0, 0, 255), 3)
+	cv2.putText(image, "dx: {}, dy: {}".format(dX, dY),
+		(10, image.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+		0.35, (0, 0, 255), 1)
+	#Motor
+	#Read
+	dxl_present_position, dxl_comm_result, dxl_error = packetHandler.read2ByteTxRx(portHandler, DXL_ID, ADDR_PRO_PRESENT_POSITION)
+        if dxl_comm_result != COMM_SUCCESS:
+            print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+        elif dxl_error != 0:
+            print("%s" % packetHandler.getRxPacketError(dxl_error))
+
+        print("[ID:%03d] GoalPos:%03d  PresPos:%03d" % (DXL_ID, dxl_goal_position[index], dxl_present_position))
+
+        if not abs(dxl_goal_position[index] - dxl_present_position) > DXL_MOVING_STATUS_THRESHOLD:
+            break
+	# Write
+	dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_PRO_GOAL_POSITION, dxl_goal_position)
+	if dxl_comm_result != COMM_SUCCESS:
+            print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+        elif dxl_error != 0:
+            print("%s" % packetHandler.getRxPacketError(dxl_error))
+
+        
+	# show the frame to our screen
+	cv2.imshow("Frame", image)
+	key = cv2.waitKey(1) & 0xFF
+	counter+=1
+        # clear the stream in preparation for the next frame
+	rawCapture.truncate(0)
+	
+	# if the 'q' key is pressed, stop the loop
+	if key == ord("q"):
+		break
+ 
+# close all windows
+cv2.destroyAllWindows()
+
+# Disable Dynamixel Torque
+dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_PRO_TORQUE_ENABLE, TORQUE_DISABLE)
+if dxl_comm_result != COMM_SUCCESS:
+    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+elif dxl_error != 0:
+    print("%s" % packetHandler.getRxPacketError(dxl_error))
+
+# Close port
+portHandler.closePort()
