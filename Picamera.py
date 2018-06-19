9c7019b68dfb1abaf1588548a4dc486aab275c06
Yunchu <33641269+YunchuZhang@users.noreply.github.com>
2018-06-19 11:45:15 -0700

    Add files via upload

--- Picamera.py
+++ Picamera.py
@@ -1,47 +1,6 @@
 # import the necessary packages
-import os
-import time
 import math
-if os.name == 'nt':
-	import msvcrt
-	def getch():
-		return msvcrt.getch().decode()
-else:
-	import sys, tty, termios
-	fd = sys.stdin.fileno()
-	old_settings = termios.tcgetattr(fd)
-	def getch():
-		try:
-			tty.setraw(sys.stdin.fileno())
-			ch = sys.stdin.read(1)
-		finally:
-			termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-		return ch
-
-# Control table address
-ADDR_PRO_TORQUE_ENABLE      = 24              # Control table address is different in Dynamixel model
-ADDR_PRO_GOAL_POSITION      = 30
-ADDR_PRO_PRESENT_POSITION   = 37
-
-# Protocol version
-PROTOCOL_VERSION            = 2.0               # See which protocol version is used in the Dynamixel
-
-# Default setting
-DXL_ID                      = 11                 # Dynamixel ID : 1
-BAUDRATE                    = 1000000             # Dynamixel default baudrate : 57600
-DEVICENAME                  = '/dev/ttyUSB0'    # Check which port is being used on your controller
-                                                # ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
-
-TORQUE_ENABLE               = 1                 # Value for enabling the torque
-TORQUE_DISABLE              = 0                 # Value for disabling the torque
-# mid 515
-DXL_MINIMUM_POSITION_VALUE  = 206          # Dynamixel will rotate between this value
-DXL_MAXIMUM_POSITION_VALUE  = 824            # and this value (note that the Dynamixel would not move when the position value is out of movable range. Check e-manual about the range of the Dynamixel you use.)
-DXL_MOVING_STATUS_THRESHOLD = 20                # Dynamixel moving status threshold
-
-
-
-from dynamixel_sdk import * 
+from dynamixel import * 
 from collections import deque
 import numpy as np
 from picamera.array import PiRGBArray
