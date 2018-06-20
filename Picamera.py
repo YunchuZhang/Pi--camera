f86127b3de8a2eeededa2b4e25bdccdfc7ca9f91
Yunchu <33641269+YunchuZhang@users.noreply.github.com>
2018-06-19 17:18:41 -0700

    Add files via upload

--- Picamera.py
+++ Picamera.py
@@ -90,158 +90,158 @@ elif dxl_error != 0:
 
 # allow the camera or video file to warm up
 time.sleep(0.2)
-# capture frames from the camera
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-	# grab the raw NumPy array representing the image, then initialize the timestamp
-	# and occupied/unoccupied text
-	image = frame.array
+# # capture frames from the camera
+# for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+# 	# grab the raw NumPy array representing the image, then initialize the timestamp
+# 	# and occupied/unoccupied text
+# 	image = frame.array
 
 
-	# resize the frame, blur it, and convert it to the HSV
-	# color space
+# 	# resize the frame, blur it, and convert it to the HSV
+# 	# color space
 	
-	blurred = cv2.GaussianBlur(image, (11, 11), 0)
-	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+# 	blurred = cv2.GaussianBlur(image, (11, 11), 0)
+# 	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
  
-	# construct a mask for the color "green", then perform
-	# a series of dilations and erosions to remove any small
-	# blobs left in the mask
-	mask = cv2.inRange(hsv, redLower, redUpper)
-	mask = cv2.erode(mask, None, iterations=2)
-	mask = cv2.dilate(mask, None, iterations=2)
-        # find contours in the mask and initialize the current
-	# (x, y) center of the ball
-	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-		cv2.CHAIN_APPROX_SIMPLE)
-	cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-	center = None
+# 	# construct a mask for the color "green", then perform
+# 	# a series of dilations and erosions to remove any small
+# 	# blobs left in the mask
+# 	mask = cv2.inRange(hsv, redLower, redUpper)
+# 	mask = cv2.erode(mask, None, iterations=2)
+# 	mask = cv2.dilate(mask, None, iterations=2)
+#         # find contours in the mask and initialize the current
+# 	# (x, y) center of the ball
+# 	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+# 		cv2.CHAIN_APPROX_SIMPLE)
+# 	cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+# 	center = None
  
-	# only proceed if at least one contour was found
-	if len(cnts) > 0:
-		# find the largest contour in the mask, then use
-		# it to compute the minimum enclosing circle and
-		# centroid
-		c = max(cnts, key=cv2.contourArea)
-		((x, y), radius) = cv2.minEnclosingCircle(c)
-		M = cv2.moments(c)
-		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+# 	# only proceed if at least one contour was found
+# 	if len(cnts) > 0:
+# 		# find the largest contour in the mask, then use
+# 		# it to compute the minimum enclosing circle and
+# 		# centroid
+# 		c = max(cnts, key=cv2.contourArea)
+# 		((x, y), radius) = cv2.minEnclosingCircle(c)
+# 		M = cv2.moments(c)
+# 		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
  
-		# only proceed if the radius meets a minimum size
-		if radius > 5:
-			# draw the circle and centroid on the frame,
-			# then update the list of tracked points
-			cv2.circle(image, (int(x), int(y)), int(radius),
-				(0, 255, 255), 2)
-			cv2.circle(image, center, 5, (0, 0, 255), -1)
-			pts.appendleft(center)
+# 		# only proceed if the radius meets a minimum size
+# 		if radius > 5:
+# 			# draw the circle and centroid on the frame,
+# 			# then update the list of tracked points
+# 			cv2.circle(image, (int(x), int(y)), int(radius),
+# 				(0, 255, 255), 2)
+# 			cv2.circle(image, center, 5, (0, 0, 255), -1)
+# 			pts.appendleft(center)
 
-	# loop over the set of tracked points
-	for i in range(1, len(pts)):
-		# if either of the tracked points are None, ignore
-		# them
-		if pts[i - 1] is None or pts[i] is None:
-			continue
+# 	# loop over the set of tracked points
+# 	for i in range(1, len(pts)):
+# 		# if either of the tracked points are None, ignore
+# 		# them
+# 		if pts[i - 1] is None or pts[i] is None:
+# 			continue
 
-		# check to see if enough points have been accumulated in
-		# the buffer
-		if counter >= 3 and i == 1 and pts[-3] is not None:
-			# compute the difference between the x and y
-			# coordinates and re-initialize the direction
-			# text variables
-			for j in range(-3,i+1):
-				dX += pts[j][0]
-				dY += pts[j][1]
-			dX = int(dX/5)
-			dY = int(dY/5)
-			# (dirX, dirY) = ("", "")
+# 		# check to see if enough points have been accumulated in
+# 		# the buffer
+# 		if counter >= 3 and i == 1 and pts[-3] is not None:
+# 			# compute the difference between the x and y
+# 			# coordinates and re-initialize the direction
+# 			# text variables
+# 			for j in range(-3,i+1):
+# 				dX += pts[j][0]
+# 				dY += pts[j][1]
+# 			dX = int(dX/5)
+# 			dY = int(dY/5)
+# 			# (dirX, dirY) = ("", "")
  
-			# ensure there is significant movement in the
-			# x-direction
-			# if np.abs(dX) > 20:
-			# 	dirX = "East" if np.sign(dX) == 1 else "West"
+# 			# ensure there is significant movement in the
+# 			# x-direction
+# 			# if np.abs(dX) > 20:
+# 			# 	dirX = "East" if np.sign(dX) == 1 else "West"
  
-			# # ensure there is significant movement in the
-			# # y-direction
-			# if np.abs(dY) > 20:
-			# 	dirY = "North" if np.sign(dY) == 1 else "South"
+# 			# # ensure there is significant movement in the
+# 			# # y-direction
+# 			# if np.abs(dY) > 20:
+# 			# 	dirY = "North" if np.sign(dY) == 1 else "South"
  
-			# # handle when both directions are non-empty
-			# if dirX != "" and dirY != "":
-			# 	direction = "{}-{}".format(dirY, dirX)
+# 			# # handle when both directions are non-empty
+# 			# if dirX != "" and dirY != "":
+# 			# 	direction = "{}-{}".format(dirY, dirX)
  
-			# # otherwise, only one direction is non-empty
-			# else:
-			# 	direction = dirX if dirX != "" else dirY
+# 			# # otherwise, only one direction is non-empty
+# 			# else:
+# 			# 	direction = dirX if dirX != "" else dirY
 
  
-		# otherwise, compute the thickness of the line and
-		# draw the connecting lines
-		print("Dx: %3f  Dy: %3f"%(dX,dY))
-		thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-		cv2.line(image, pts[i - 1], pts[i], (0, 0, 255), thickness)
+# 		# otherwise, compute the thickness of the line and
+# 		# draw the connecting lines
+# 		print("Dx: %3f  Dy: %3f"%(dX,dY))
+# 		thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+# 		cv2.line(image, pts[i - 1], pts[i], (0, 0, 255), thickness)
 
-	# show the movement deltas and the direction of movement on
-	# the frame
-	cv2.putText(image, direction, (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
-		0.65, (0, 0, 255), 3)
-	cv2.putText(image, "dx: {}, dy: {}".format(dX, dY),
-		(10, image.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
-		0.35, (0, 0, 255), 1)
-	#Motor
-	# PID
-	Perr = 1 * (dX - setx)
-	#Read
-	dxl_goal_position = int (Perr * 0.9656 + 515) 
-	if dxl_goal_position > 824:
-		dxl_goal_position = 824
-	elif dxl_goal_position < 206:
-		dxl_goal_position = 206
+# 	# show the movement deltas and the direction of movement on
+# 	# the frame
+# 	cv2.putText(image, direction, (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
+# 		0.65, (0, 0, 255), 3)
+# 	cv2.putText(image, "dx: {}, dy: {}".format(dX, dY),
+# 		(10, image.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+# 		0.35, (0, 0, 255), 1)
+# 	#Motor
+# 	# PID
+# 	Perr = 1 * (dX - setx)
+# 	#Read
+# 	dxl_goal_position = int (Perr * 0.9656 + 515) 
+# 	if dxl_goal_position > 824:
+# 		dxl_goal_position = 824
+# 	elif dxl_goal_position < 206:
+# 		dxl_goal_position = 206
 
-	dxl_present_position, dxl_comm_result, dxl_error = packetHandler.read2ByteTxRx(portHandler, DXL_ID, ADDR_PRO_PRESENT_POSITION)
-	if dxl_comm_result != COMM_SUCCESS:
-		print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-	elif dxl_error != 0:
-		print("%s" % packetHandler.getRxPacketError(dxl_error))
-		print("[ID:%03d] GoalPos:%03d  PresPos:%03d" % (DXL_ID, dxl_goal_position, dxl_present_position))
-	if abs(dxl_goal_position - dxl_present_position) > 3:
-	#Image feedback	
-	# Write
-		#x 0~640 y 0~480
-		#206 515 824
-		# -320 ~320 
-		#P = P * (dX * 0.9656 + 206 - dxl_present_position)
+# 	dxl_present_position, dxl_comm_result, dxl_error = packetHandler.read2ByteTxRx(portHandler, DXL_ID, ADDR_PRO_PRESENT_POSITION)
+# 	if dxl_comm_result != COMM_SUCCESS:
+# 		print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+# 	elif dxl_error != 0:
+# 		print("%s" % packetHandler.getRxPacketError(dxl_error))
+# 		print("[ID:%03d] GoalPos:%03d  PresPos:%03d" % (DXL_ID, dxl_goal_position, dxl_present_position))
+# 	if abs(dxl_goal_position - dxl_present_position) > 3:
+# 	#Image feedback	
+# 	# Write
+# 		#x 0~640 y 0~480
+# 		#206 515 824
+# 		# -320 ~320 
+# 		#P = P * (dX * 0.9656 + 206 - dxl_present_position)
 
 		
-		dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_PRO_GOAL_POSITION, dxl_goal_position)
-		if dxl_comm_result != COMM_SUCCESS:
-			print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-		elif dxl_error != 0:
-			print("%s" % packetHandler.getRxPacketError(dxl_error))
+# 		dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_PRO_GOAL_POSITION, dxl_goal_position)
+# 		if dxl_comm_result != COMM_SUCCESS:
+# 			print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+# 		elif dxl_error != 0:
+# 			print("%s" % packetHandler.getRxPacketError(dxl_error))
 
 
 	
 
 
-	# show the frame to our screen
-	cv2.imshow("Frame", image)
-	key = cv2.waitKey(1) & 0xFF
-	counter+=1
-        # clear the stream in preparation for the next frame
-	rawCapture.truncate(0)
+# 	# show the frame to our screen
+# 	cv2.imshow("Frame", image)
+# 	key = cv2.waitKey(1) & 0xFF
+# 	counter+=1
+#         # clear the stream in preparation for the next frame
+# 	rawCapture.truncate(0)
 	
-	# if the 'q' key is pressed, stop the loop
-	if key == ord("q"):
-		break
+# 	# if the 'q' key is pressed, stop the loop
+# 	if key == ord("q"):
+# 		break
  
-# close all windows
-cv2.destroyAllWindows()
+# # close all windows
+# cv2.destroyAllWindows()
 
-# Disable Dynamixel Torque
-dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_PRO_TORQUE_ENABLE, TORQUE_DISABLE)
-if dxl_comm_result != COMM_SUCCESS:
-    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-elif dxl_error != 0:
-    print("%s" % packetHandler.getRxPacketError(dxl_error))
+# # Disable Dynamixel Torque
+# dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_PRO_TORQUE_ENABLE, TORQUE_DISABLE)
+# if dxl_comm_result != COMM_SUCCESS:
+#     print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+# elif dxl_error != 0:
+#     print("%s" % packetHandler.getRxPacketError(dxl_error))
 
-# Close port
-portHandler.closePort()
+# # Close port
+# portHandler.closePort()
