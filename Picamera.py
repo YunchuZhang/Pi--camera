7ee83479155492dc1b6a3fdd1693ffedbfe3c52d
Yunchu <33641269+YunchuZhang@users.noreply.github.com>
2018-06-22 10:35:57 -0700

    Add files via upload

--- Picamera.py
+++ Picamera.py
@@ -92,7 +92,7 @@ for ID in range(11,15):
 	if dxl_comm_result != COMM_SUCCESS:
 		print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
 	elif dxl_error != 0:
-    	print("%s" % packetHandler.getRxPacketError(dxl_error))
+		print("%s" % packetHandler.getRxPacketError(dxl_error))
 	else:
 		print("Dynamixel has been successfully connected")
 
