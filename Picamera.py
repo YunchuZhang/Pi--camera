f2b1567e08b1654dd40ebc5bc0d851bb6699f166
Yunchu <33641269+YunchuZhang@users.noreply.github.com>
2018-06-22 19:40:17 -0700

    Add files via upload

--- Picamera.py
+++ Picamera.py
@@ -288,23 +288,27 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 	#0 ~512 ~1024
 	#-90 ~0 ~ 90
 	# IK
-	basepoint =[trans[0][0],trans[1][0],trans[2][0]]
-	settheta[0] = np.arctan2(basepoint[1], basepoint[0])
-	settheta[0] = int (settheta[0]*180/PI)
-	n = basepoint[0]**2 + basepoint[1]**2
-	m = basepoint[2] - 65
-	n = np.sqrt(n)
-	print(m)
-	print(n)
-	print(- m**2 - n**2 + 129*129 + 65*65)
-	print(2*129*65)
-	print(np.arccos((- m**2 - n**2 + 129*129 + 65*65)/(2*129*65)))
-	settheta[2] = np.arccos(((- m**2 - n**2 + 129*129 + 65*65)/(2*129*65)))
-	settheta[2] = 180 - int (settheta[2]*180/PI) - 37
-	belta = np.arctan2(m,n)
-	fi = np.arccos(((m**2 + n**2 + 129*129 - 65*65)/(2*129 * np.sqrt(n**2 +m**2))))
-	settheta[1] = int ((belta - fi)*180/PI) + 70
-	settheta[3] = 90 - settheta[1] -settheta[2] - 90
+	if begin == 1:
+		basepoint =[trans[0][0],trans[1][0],trans[2][0]]
+		settheta[0] = np.arctan2(basepoint[1], basepoint[0])
+		settheta[0] = int (settheta[0]*180/PI)
+		n = basepoint[0]**2 + basepoint[1]**2
+		m = basepoint[2] - 65
+		n = np.sqrt(n)
+		print(m)
+		print(n)
+		print(- m**2 - n**2 + 129*129 + 65*65)
+		print(2*129*65)
+		print(np.arccos((- m**2 - n**2 + 129*129 + 65*65)/(2*129*65)))
+		settheta[2] = np.arccos(((- m**2 - n**2 + 129*129 + 65*65)/(2*129*65)))
+		settheta[2] = 180 - int (settheta[2]*180/PI) - 37
+		belta = np.arctan2(m,n)
+		fi = np.arccos(((m**2 + n**2 + 129*129 - 65*65)/(2*129 * np.sqrt(n**2 +m**2))))
+		settheta[1] = int ((belta - fi)*180/PI) + 70
+		settheta[3] = 90 - settheta[1] -settheta[2] - 90
+	else :
+		for i in range(0,4):
+			settheta[i] = 0
 	for i in range(0,4):
 		settheta[i] = int (settheta[i] * 5.688 + 512)
 	for ID in range(11,15):
