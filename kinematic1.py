96e157f5e4c0342d4f2cc8ba0fbbfbe0a7f0ec98
Yunchu <33641269+YunchuZhang@users.noreply.github.com>
2018-07-09 16:53:34 -0700

    Add files via upload

---
+++ kinematic1.py
@@ -0,0 +1,27 @@
+import numpy as np
+import math
+PI = np.pi
+#trans_mat = np.eye(4)
+#end effect -51
+def tmatrix1 (a,alpha,theta,d):
+	temp = np.array([[np.cos(theta), -np.sin(theta)*np.cos(alpha), np.sin(theta)*np.sin(alpha), a*np.cos(theta)],
+	[np.sin(theta), np.cos(theta)*np.cos(alpha), -np.cos(theta)*np.sin(alpha), a*np.sin(theta)],
+	[0, np.sin(alpha), np.cos(alpha), d],
+	[0, 0, 0, 1]])
+	return temp
+def goalpos1(theta1,theta2,theta3,theta4):
+	trans_mat = np.eye(4)
+	t1 = tmatrix1(0,-PI * 0.5,theta1,65)
+	t2 = tmatrix1(129,0,-PI*(156/180.0)+theta2,0)
+	t3 = tmatrix1(65,0,(99/180.0)*PI+theta3,0)
+	t4 = tmatrix1(83,0,(61/180.0)*PI+theta4,0)
+	trans_mat = np.dot(trans_mat,t1)
+	#print(trans_mat)
+	trans_mat = np.dot(trans_mat,t2)
+	#print(trans_mat)
+	trans_mat = np.dot(trans_mat,t3)
+	#print(trans_mat)
+	trans_mat = np.dot(trans_mat,t4)
+	#print(trans_mat)
+	return trans_mat
+#print(np.dot(goalpos1(0,(0/180)*PI,(0/180)*PI,(0/180)*PI),[[0],[0],[0],[1]]))
--- tinyik.py
+++ tinyik.py
@@ -1,2 +1,25 @@
-import tinyik
+from core import *
 import numpy as ny
+from kinematic1 import *
+arm = Actuator(['z',[129.0,0.,0.], 'z',[65.0,0.,0.],'z',[83.0,0.,0.]])
+arm.angles =[-PI*(156/180.0),(99/180.0)*PI,(61/180.0)*PI]
+print(arm.ee)
+settheta = [0,0,0,0]
+trans = np.dot(goalpos1(0,1.57,-0.57,0),[[100],[0],[0],[1]])
+print(trans)
+basepoint =np.array([trans[0][0],trans[1][0],trans[2][0]])
+settheta[0] = np.arctan2(basepoint[1], basepoint[0])
+settheta[0] = int (settheta[0]*180/PI)
+
+arm.angles =[-PI*(156/180.0)+1.57,(99/180.0)*PI-0.57,(61/180.0)*PI]
+print(arm.ee)
+#arm.ee = []
+arm.ee=[basepoint[0],65-basepoint[2],basepoint[1]]
+(settheta[1],settheta[2],settheta[3]) = np.round(np.rad2deg(arm.angles))
+print(np.round(np.rad2deg(arm.angles)))
+
+settheta[1] = settheta[1]+ 4 + 90
+settheta[2] = settheta[2] - 38
+settheta[3] = settheta[3] - 90
+print(settheta)
+print(arm.ee)
